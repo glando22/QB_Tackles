b1db6e2f6ea0d98fa010aeb7a84fb63800d3dbfc
Greg Landis <greg.landis22@outlook.com>
2026-04-09 11:50:49 -0500

    Refactor dashboard.py for improved readability

--- dashboard.py
+++ dashboard.py
@@ -169,7 +169,6 @@ elif page == "Team Profiles":
     incomplete_weeks = [t for t in tackles if "message" in t]
     if len(incomplete_weeks) == 18 and year != "All-time":
         st.info("The season hasn't started yet. Check back once the season starts to see how your team is doing!")
-        st.stop()
     team_tackles = [t for t in tackles if "message" not in t and t["owner"]["user_id"] == owner]
     if team_tackles:
         for t in team_tackles:
